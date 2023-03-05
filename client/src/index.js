fb1f5d5a037fa18ad3e859e263308c16f7f35f3b
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-03-05 18:51:32 +0900

    correction

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import reactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
