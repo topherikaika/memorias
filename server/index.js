10af3781a795dac13c67cfe88a901147fbbebe6b
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-03-05 19:24:29 +0900

    set-up mongoose for any warnings in console

--- server/index.js
+++ server/index.js
@@ -13,3 +13,7 @@ const CONNECTION_URL = 'mongodb + srv://memorias:memorias123@cluster0.kohzkj0.mo
 const port = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModifyToFalse', false);
