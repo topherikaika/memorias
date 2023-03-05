0343fee81f6623683f1321e63baccff56ffb0bb0
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-03-05 19:16:19 +0900

    create access credentials for db

--- server/index.js
+++ server/index.js
@@ -9,4 +9,4 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = mongodb + srv://<username>:<password>@cluster0.kohzkj0.mongodb.net/?retryWrites=true&w=majority;
+const CONNECTION_URL = 'mongodb + srv://memorias:memorias123@cluster0.kohzkj0.mongodb.net/?retryWrites=true&w=majority;
