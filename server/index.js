f39364bb854e186530d1acf2d7758f1b1e746e28
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-03-05 19:39:03 +0900

    changed server to port 3000

--- server/index.js
+++ server/index.js
@@ -9,11 +9,11 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb + srv://memorias:memorias123@cluster0.kohzkj0.mongodb.net/?retryWrites=true&w=majority';
-const port = process.env.PORT || 5000;
+const CONNECTION_URL = 'mongodb+srv://memorias:memorias123@cluster0.kohzkj0.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
   .catch((error) => console.log(error.message));
 
-mongoose.set('useFindAndModifyToFalse', false);
+//mongoose.set('useFindAndModify', false);
