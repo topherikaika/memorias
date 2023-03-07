f2f91214d2958dd80b6c1f64b69771d4a7cc2185
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-03-07 09:48:02 +0900

    create return for all postMEssages

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -4,6 +4,7 @@ export const getPosts = async (req, res) => {
   try {
     const postMessages = await PostMessage.find();
     console.log(postMessages);
+    res.status(200).json(postMessages);
   } catch (error) {
 
   }
