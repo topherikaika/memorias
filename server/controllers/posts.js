59cc7be117cdff0eaf1159bd5de2e33e2af7fd12
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-03-07 09:46:31 +0900

    create console.log for postMessages

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -3,6 +3,7 @@ import PostMessage from '../models/postMessage';
 export const getPosts = async (req, res) => {
   try {
     const postMessages = await PostMessage.find();
+    console.log(postMessages);
   } catch (error) {
 
   }
