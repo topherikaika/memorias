8ed79202a73d652e1c9e25a445ca67f019f612f3
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-03-07 09:39:59 +0900

    create route post for createPost

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -5,6 +5,6 @@ import { getPosts } from '../controllers/posts.js';
 const router = express.Router();
 
 router.get('/', getPosts);
-router.post('/', getPosts);
+router.post('/', createPost);
 
 export default router;
